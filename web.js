0bf0caceb27fbf017834310f19d83b93dc77c9ac
Monique de Haas <monique@dondersteen.net>
2013-07-08 14:10:34 +0000

    Updated fs.readFileSync to a function with err, data end callback for error and else console log the data

--- web.js
+++ web.js
@@ -5,13 +5,16 @@ var buf = new Buffer (225);
 
 app.get('/', function (request, response){
 
-request.fs.readFileSync(index.html);
+fs.readFileSync('~/bitstarter/index.html', function (err, data){
+if (err) throw err;
+console.log(data);
+});
 
 buffer.toString('utf-8');
 
 //len = buf.write('\u00bd + \u00bc = \u00be', 0);
 //console.log(len + " bytes: " + buf.toString('utf8', 0, len));
-request.pipe(response);
+//request.pipe(response);
 
 
 //response.send ("HellO World 2!");
