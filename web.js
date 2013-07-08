f5798446ceef494ddab440f510c0d5e5b38fd286
Monique de Haas <monique@dondersteen.net>
2013-07-08 15:44:48 +0000

    looked at the forum and took some tips ;)

--- web.js
+++ web.js
@@ -1,26 +1,24 @@
 var express = require ('express');
 var fs = require ('fs');
 var app = express.createServer(express.logger());
-var buf = new Buffer (225);
+var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
 
 app.get('/', function (request, response){
 
-response.send(buffer.toString('utf-8', fs.readFileSync('index.html', function (err,data) {
-if (err) throw err;
-console.log (data);})));
+response.send(buf.toString());
+});
+//response.send(buffer.toString('utf-8', fs.readFileSync('index.html', function (err,data) {
+//if (err) throw err;
+//console.log (data);})));
 
 //fs.readFileSync('index.html', function (err, data){
 //if (err) throw err;
 //console.log(data);
-});
-
-buffer.toString('utf-8');
-
-len = buf.write('\u00bd + \u00bc = \u00be', 0);
-console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+//});
+//buffer.toString('utf-8');
+//len = buf.write('\u00bd + \u00bc = \u00be', 0);
+//console.log(len + " bytes: " + buf.toString('utf8', 0, len));
 //request.pipe(response);
-
-
 //response.send ("HellO World 2!");
 //(end);
 
