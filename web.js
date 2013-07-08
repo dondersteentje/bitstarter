973891552b4dfb6cc4cdb134a51c8ab8fa24d9e0
Monique de Haas <monique@dondersteen.net>
2013-07-08 15:07:20 +0000

    renewed effort to solve the node code

--- web.js
+++ web.js
@@ -5,20 +5,24 @@ var buf = new Buffer (225);
 
 app.get('/', function (request, response){
 
-fs.readFileSync('index.html', function (err, data){
+response.send(buffer.toString('utf-8', fs.readFileSync('index.html', function (err,data) {
 if (err) throw err;
-console.log(data);
+console.log (data);})));
+
+//fs.readFileSync('index.html', function (err, data){
+//if (err) throw err;
+//console.log(data);
 });
 
 buffer.toString('utf-8');
 
-//len = buf.write('\u00bd + \u00bc = \u00be', 0);
-//console.log(len + " bytes: " + buf.toString('utf8', 0, len));
-//request.pipe(response);
+len = buf.write('\u00bd + \u00bc = \u00be', 0);
+console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+request.pipe(response);
 
 
 //response.send ("HellO World 2!");
-});
+//(end);
 
 var port = process.env. PORT || 5000;
 app.listen(port, function() {
