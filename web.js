aca54ef5e3256ef65eba70aac7415ea733eae0fb
Monique de Haas <monique@dondersteen.net>
2013-07-08 13:31:23 +0000

    Update of the code to read index.html

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
-var express = require('express');
-
+var express = require ('express');
+var fs = require ('fs');
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('/', function (request, response){
+request.fs.readFileSync(index.html);
+buf = new Buffer(256);
+len = buf.write('\u00bd + \u00bc = \u00be', 0);
+console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+request.pipe(response);
+
+
+//response.send ("HellO World 2!");
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env. PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log ("Listening on " +port);
 });
